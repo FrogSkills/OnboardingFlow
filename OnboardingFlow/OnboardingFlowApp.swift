816e09d080876b25f5a225ac8a2316dbfdee9032
Miguel <mgamez20@icloud.com>
2025-06-04 19:26:49 -0400

    Initial Commit

---
+++ OnboardingFlow/OnboardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowApp.swift
+//  OnboardingFlow
+//
+//  Created by Miguel on 6/4/25.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
